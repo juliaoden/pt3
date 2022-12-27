86d7dd6f52418517c0fa3582a053bbae1968acd9
juliaoden <julia_oden@web.de>
2022-12-27 17:43:18 +0100

    Cleaned up Code

--- src/main/java/org/example/Dijkstra.java
+++ src/main/java/org/example/Dijkstra.java
@@ -4,16 +4,24 @@ import java.util.*;
 
 public class Dijkstra {
 
-    static int n = 4;
+    static int n = 4; // Number of nodes
 
-    static ArrayList<Integer> visitedNodes = new ArrayList();
+    static ArrayList<Integer> visitedNodes = new ArrayList(); // Serie of nodes
 
-    static ArrayList<Integer>[] graphArray = new ArrayList[n];
-    static Boolean[] visited = new Boolean[graphArray.length];
+    static ArrayList<Integer>[] graphArray = new ArrayList[n]; // Adjacency matrix
+    static Boolean[] visited = new Boolean[graphArray.length]; // Visited Array
+
+    public void initialize(){
+        for (int i = 0; i < n; i++) {
+            graphArray[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < n; i++) {
+            visited[i] = false;
+        }
+    }
 
     public void setMaxInt(ArrayList<Integer>[] graph) {
         for (int i = 0; i < graph.length; i++) {
-            System.out.println(graph[i]);
             for (int j = 0; j < graph.length - 1; j++) {
                 if (graph[i].get(j) == 0) {
                     graph[i].set(j, Integer.MAX_VALUE);
@@ -22,45 +30,41 @@ public class Dijkstra {
         }
     }
 
-    public void update(ArrayList<Integer>[] graph, int currentNode, Boolean[] visited) {
+    public void dijkstra(ArrayList<Integer>[] graph, int currentNode, Boolean[] visited) {
         int nextNode;
+        // Current node is marked as visited
         visited[currentNode] = true;
         for (int i = 0; i < graph.length; i++) {
-            System.out.println("We are looking at row " + currentNode);
-            //visitedNodes.add(currentNode);
-            nextNode = getNextVertex(graph, visited, currentNode); // a: nextNode = d// d: nextNode = c
-            System.out.println("Next Node is: " + nextNode);
-            if(nextNode != -1) visited[nextNode] = true; // visited = [true, flase, false, true] // visited = [true, true, false, true]
-            //int nextNode2 = getNextVertex(graph, visited, nextNode); // nächster Knoten für d = c // nächster Knoten für c = b oder so
+            // Searching for next node
+            nextNode = getNextVertex(graph, visited, currentNode);
+            // Marking next node as visited
+            if(nextNode != -1) visited[nextNode] = true;
+            // Add next node to path of nodes
             visitedNodes.add(currentNode);
+            // Repeat process with next node
             currentNode = nextNode;
         }
-
-        //return visitedNodes;
 
     }
 
 
     public int getNextVertex(ArrayList<Integer>[] graph, Boolean[] visited, int currentNode) {
-        int curBestWeight, nextWeight, x, bestNode = currentNode, bestWeight;
+        int curWeight, nextWeight, x, bestNode = currentNode;
         for (int j = 0; j < graph.length - 1; j++) {
-            // i = Zeile, j= Spalte
-            curBestWeight = graph[currentNode].get(j);
-            nextWeight = graph[currentNode].get(j + 1);
-            System.out.printf("Current node(curBestWeight): %d, Next Node(nextWeight): %d, Value of visited: %b%n", curBestWeight, nextWeight, visited[j]);
-            //System.out.println((nextWeight < curBestWeight) && (nextWeight != 0) && !visited[currentNode]);
-            // Prüfen, ob nächstes Gewicht besser ist -> MUSS NOCH ÜBERARBEITET WERDEN WG FEHLENDEN EINTRÄGEN
-            if ((nextWeight < curBestWeight) && (nextWeight != 0) && !visited[j]) {
-                //curBestWeight = nextWeight;
+            curWeight = graph[currentNode].get(j); // current weight
+            nextWeight = graph[currentNode].get(j + 1); // next weight
+            // Check if next weight is less than current weight and if it isn´t already visited
+            if ((nextWeight < curWeight) && (nextWeight != 0) && !visited[j]) {
+                // best node is the next node
                 bestNode = j + 1;
-                //System.out.println(x + "/" + y);
-            } else {
-                System.out.println("Vergleich fehlgeschlagen");
             }
         }
 
+        // the current node is the penultimate node
         if(bestNode == currentNode){
-            return java.util.Arrays.asList(visited).indexOf(Boolean.FALSE);  // da wo halt visited noch false ist
+            // find the node which isn´t already visited
+            // TODO: Was passiert, wenn letzter nicht besuchter Knoten keine Verbundung zum derzeitigen Knoten hat?
+            return java.util.Arrays.asList(visited).indexOf(Boolean.FALSE);
         } else{
             return bestNode;
         }
@@ -68,17 +72,12 @@ public class Dijkstra {
     }
 
 
-    public static void main(String[] args) {
+    public void start(Dijkstra d) {
 
         // initializing
-        for (int i = 0; i < n; i++) {
-            graphArray[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < n; i++) {
-            visited[i] = false;
-        }
+        d.initialize();
 
-        //al[0].add(1);
+        // TODO: Vom Nutzenden eingeben lassen
         graphArray[0].add(0);
         graphArray[0].add(3);
         graphArray[0].add(2);
@@ -97,10 +96,12 @@ public class Dijkstra {
         graphArray[3].add(0);
 
 
-        Dijkstra d = new Dijkstra();
+        // Replace all zeros with MAX_INT
         d.setMaxInt(graphArray);
-        d.update(graphArray, 0, visited);
+        // Start Dijkstra Algorithm
+        d.dijkstra(graphArray, 0, visited);
 
+        System.out.print("Reihenfolge der Häuser ist ");
         for (int i = 0; i < visitedNodes.size(); i++) {
             System.out.print(visitedNodes.get(i));
         }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.print("Welches Problem der Graphschaft Schilda wollen Sie angehen? \n"
+                + "1: Straßen müssen her \n"
+                + "2: Wasserversorgung \n"
+                + "3: Stromversorgung \n"
+                + "4: Historische Funde \n"
+                + "5: Die Festhochzeit – das Verteilen der Einladungen \n"
+                + "6: Wohin nur mit den Gästen? \n"
+                + "7: Es gibt viel zu tun! Wer macht‘s? \n"
+                + "Geben Sie die Nummer des gewünschten Problems ein! \n"
+        );
+        int problem = input.nextInt();
+
+        switch (problem) {
+            // Problem: Straßen müssen her
+            case 1:
+                Dijkstra d = new Dijkstra();
+                d.start(d);
+                break;
+            // Problem: Wasserversorgung
+            case 2:
+                break;
+            // Problem: Stromversorgung
+            case 3:
+                break;
+            // Problem: Historische Funde
+            case 4:
+                break;
+            // Problem: Die Festhochzeit – das Verteilen der Einladungen
+            case 5:
+                break;
+            // Problem: Wohin nur mit den Gästen?
+            case 6:
+                break;
+            // Problem: Es gibt viel zu tun! Wer macht‘s?
+            case 7:
+                break;
+        }
+
     }
 }
